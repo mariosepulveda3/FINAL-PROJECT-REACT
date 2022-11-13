77f4de8e5a2444c012d9368b12fca9c04798a47a
mariosepulveda3 <mariosepul3@gmail.com>
2022-11-13 17:38:38 +0100

    REFACTOR: moves search movie logic from movie component to Search page

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Movie = ({ movie, search }) => {
+const Movie = ({ movie }) => {
   return (
     <div className="movie">
-      {movie.title.toLowerCase().includes(search) ? (
+      {
         <div className="container">
           <p>{movie.title}</p>
           <img src={movie.img} alt={movie.title} referrerPolicy="no-referrer" />
         </div>
-      ) : null}
+      }
     </div>
   );
 };
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,40 +2,41 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Movie from "../components/Movie";
+import { getMovies } from "../redux/movies/movies.functions";
 
 const Search = () => {
-  const dispatch = useDispatch()
-  const {movies, isLoading, search, error} = useSelector(state => state.movies)
+  const dispatch = useDispatch();
+  const { movies, isLoading, search, error } = useSelector(
+    (state) => state.movies
+  );
 
   useEffect(() => {
-    const getMovies = async () => {
-      try {
-
-        dispatch({type: 'gettingMovies'});
-        const res = await axios.get("http://localhost:3000/movies");
-  
-        dispatch({type: 'getMovies', payload: res.data});
-        
-      } catch (error) {
-        dispatch({type: 'errorMovies', payload: error.message})
-      }
-    };
-    getMovies();
+    getMovies(dispatch);
   }, []);
 
   return (
     <div className="search">
       <input
         type="text"
-        onKeyUp={(e) => dispatch({type: 'searchMovies', payload: e.target.value.toLowerCase()})}
+        onKeyUp={(e) =>
+          dispatch({
+            type: "searchMovies",
+            payload: e.target.value.toLowerCase(),
+          })
+        }
         placeholder="Search movie"
       />
-      {isLoading && (<h2>Loading movies...</h2>)}
-      {error && (<h2>{error}</h2>)}
-      {movies.length > 0 &&
+      {isLoading && <h2>Loading movies...</h2>}
+      {error && <h2>{error}</h2>}
+      {movies.length > 0 && (
         <div className="divPelis">
-          {movies.map((movie) => <Movie key={JSON.stringify(movie)} movie={movie} search={search}/>)}
-        </div>}
+          {movies.map((movie) => {
+            return movie.title.toLowerCase().includes(search) && (
+              <Movie key={JSON.stringify(movie)} movie={movie} />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
