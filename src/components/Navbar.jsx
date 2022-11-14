7a5e8baec8e08eaea5fd1888422baa2764ae518e
Karuh13 <alvaro.c.s.13@gmail.com>
2022-11-14 21:04:32 +0100

    FEAT: navbar logo links to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import LogOutButton from "./LogOutButton";
 import NavBarLink from "./NavBarLink";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	const { user, token } = useSelector((state) => state.auth);
@@ -23,11 +24,13 @@ const Navbar = () => {
 					</>
 				)}
 			</div>
-			<img
-        className="logo"
-				src="https://i0.wp.com/www.movieaddicts.nl/wp-content/uploads/2015/08/LOGO-MOVIEADDICTS-1.png?fit=822%2C464&ssl=1"
-				alt="logo"
-			/>
+			<Link to="/">
+				<img
+					className="logo"
+					src="https://i0.wp.com/www.movieaddicts.nl/wp-content/uploads/2015/08/LOGO-MOVIEADDICTS-1.png?fit=822%2C464&ssl=1"
+					alt="logo"
+				/>
+			</Link>
 			<div className="user">
 				<NavBarLink route="login" name="Login" />
 				<NavBarLink route="register" name="Register" />
