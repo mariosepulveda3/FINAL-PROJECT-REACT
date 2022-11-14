ebfa28c9d28d18501b0679c09390d4957941892b
Karuh13 <alvaro.c.s.13@gmail.com>
2022-11-14 19:23:12 +0100

    FIX: bug reloading admin private routes

--- src/components/AuthRouteAdmin.jsx
+++ src/components/AuthRouteAdmin.jsx
@@ -4,6 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const AuthRouteAdmin = ({ component }) => {
 	const { user } = useSelector((state) => state.auth);
+	if (!user) return <Navigate to="/" />
 	if (user.rol !== "admin") return <Navigate to="/login" />;
 	if (user.rol === "admin") return component;
 };
